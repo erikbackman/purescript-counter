8bd2a68f8dc0873e55fdec15152bca11e56c75f5
erikbackman <erikb.github@gmail.com>
2020-07-31 00:07:37 +0200

    Cleanup Main.purs

--- src/Main.purs
+++ src/Main.purs
@@ -4,10 +4,10 @@ import Prelude
 import Effect (Effect)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
-import Component.Counter as Counter 
+import Component.Counter as Counter
 
 main :: Effect Unit
-main = do
+main =
   HA.runHalogenAff do
     body <- HA.awaitBody
     runUI Counter.component unit body
