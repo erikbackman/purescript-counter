d16f82c7a9d59d4729aa66bb4fa76fa84f3477d0
erikbackman <erikb.github@gmail.com>
2020-07-31 00:03:24 +0200

    Cleanup Counter.purs

--- src/Component/Counter.purs
+++ src/Component/Counter.purs
@@ -1,33 +1,36 @@
 module Component.Counter where
 
 import Prelude
-
 import Data.Maybe (Maybe(..))
 import Halogen (Component, defaultEval, mkComponent, mkEval, modify_)
 import Halogen.HTML (HTML, div_, p_, button, text)
 import Halogen.HTML.Events (onClick)
 
-data Action = Increment | Decrement
-  
+data Action
+  = Increment
+  | Decrement
+
 component :: forall q i o m. Component HTML q i o m
 component =
   mkComponent
     { initialState: const 0
-    , render: render 
-    , eval: mkEval defaultEval
-      { handleAction = handleAction
-      }
+    , render: render
+    , eval:
+        mkEval
+          defaultEval
+            { handleAction = handleAction
+            }
     }
-    where
-      render state =
-        div_
-        [ p_
+  where
+  render state =
+    div_
+      [ p_
           [ button [ onClick \_ -> Just Decrement ] [ text "-" ]
           , text (show state)
-          , button [ onClick \_ -> Just Increment ] [ text "+" ]  
+          , button [ onClick \_ -> Just Increment ] [ text "+" ]
           ]
-        ]
+      ]
 
-      handleAction = case _ of
-        Decrement -> modify_ \s -> s - 1
-        Increment -> modify_ \s -> s + 1 
+  handleAction = case _ of
+    Decrement -> modify_ (_ - 1)
+    Increment -> modify_ (_ + 1)
